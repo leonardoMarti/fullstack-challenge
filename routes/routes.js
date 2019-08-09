5153933e5c2ab9f542177720b2f9a74eeafbd265
Leonardo <Martinelli>
2019-08-09 18:59:56 -0300

    cria rotas vazias

---
+++ routes/routes.js
@@ -0,0 +1,33 @@
+module.exports = function(app) {
+  app.get("/urls/:id", (req, res) => {
+    res.send("/urls/:id");
+  });
+
+  app.delete("/urls/:id", (req, res) => {
+    res.send("/urls/:id");
+  });
+
+  app.post("/users", (req, res) => {
+    res.send("/users");
+  });
+
+  app.get("/users/:userId/stats", (req, res) => {
+    res.send("/users/:userId/stats");
+  });
+
+  app.delete("/user/:userid", (req, res) => {
+    res.send("/urls/:id");
+  });
+
+  app.post("/user/:userid/urls", (req, res) => {
+    res.send("/user/:userid/urls");
+  });
+
+  app.get("/stats", (req, res) => {
+    res.send("/stats");
+  });
+
+  app.get("/stats/:id", (req, res) => {
+    res.send("/stats/:id");
+  });
+};
